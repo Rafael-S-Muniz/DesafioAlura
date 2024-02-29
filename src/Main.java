d3eca26bbad7ab30b20bec65209eb070a77a6a94
Rafael-S-Muniz <rsmuniz18@gmail.com>
2024-02-29 18:15:00 -0300

    Código desafio01 alura

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
@@ -10,15 +12,56 @@ public class Main {
                 "        3- Transferir valor\n" +
                 "        4- Sair\n" +
                 "\n");
+        int operacao = scanner.nextInt();
 
         String nomeDoCliente = "Rafael Muniz";
         String conta = "Conta Corrente";
-        int operacao = scanner.nextInt();
         double saldo = 0;
 
-        while (operacao != 4)
-        {if (operacao == 1) {
-            System.out.println("O Seu saldo é de: " + saldo);
+        while (operacao != 4) {
+            if (operacao == 1) {
+                System.out.println("O Seu saldo é de: " + saldo);
+            } else if (operacao == 2) {
+                System.out.println("Qual valor gostaria de depositar?");
+                double deposito = scanner.nextDouble();
+                if (deposito >= 0) {
+                    System.out.println("Tem certeza que deseja depositar esse valor? Sim/Não.");
+                }
+                String resposta = scanner.next();
+                while (resposta.equals("Não")) {
+                    System.out.println("Qual valor gostaria de depositar?");
+                    double valorDeposito = scanner.nextDouble();
+                    if (valorDeposito >= 0) {
+                        System.out.println("Tem certeza que deseja depositar esse valor? Sim/Não.");
+                        String resposta2 = scanner.next();
+                        resposta = resposta2;
+                        deposito = valorDeposito;
+                    } else {
+                        System.out.println("Valor inválido, tente novamente");
+                    }
+                }
+                if (resposta.equals("Sim")) {
+                    saldo += deposito;
+                    System.out.println("Valor depositado com sucesso!");
+                }
+            }
+            else if (operacao == 3) {
+                System.out.println("Para qual conta gostaria de transferir?");
+                String account = scanner.next();
+                System.out.println("Qual o valor que gostaria de transferir?");
+                double valor = scanner.nextDouble();
+                if (valor >= 0) {
+                    if (valor > saldo) {
+                        System.out.println("Você não tem esse valor para transferir");
+
+                    } else if (valor <= saldo) {
+                        saldo -= valor;
+                        System.out.println("Valor transferido com sucesso para a conta: " + account);
+                    }
+                }
+            } else {
+                System.out.println("Operação inválida, tente novamente.");
+            }
 
             System.out.println("        Digite a operação desejada: \n" +
                     "        1- Consultar saldos\n" +
@@ -27,50 +70,11 @@ public class Main {
                     "        4- Sair\n" +
                     "\n");
             operacao = scanner.nextInt();
-        } else if (operacao == 2) {
-            System.out.println("Qual valor gostaria de depositar?");
-            double deposito = scanner.nextDouble();
-            System.out.println("Tem certeza que deseja depositar esse valor? Sim/Não.");
-            String resposta = scanner.next();
-            if (resposta.equals("Sim")) {
-                saldo += deposito;
-                System.out.println("Valor depositado com sucesso!");
-                System.out.println("        Digite a operação desejada: \n" +
-                        "        1- Consultar saldos\n" +
-                        "        2- Depositar valor\n" +
-                        "        3- Transferir valor\n" +
-                        "        4- Sair\n" +
-                        "\n");
-                operacao = scanner.nextInt();
-            } else if (resposta.equals("Não")) {
-                System.out.println("Qual valor gostaria de depositar?");
-                double valor = scanner.nextDouble();
-                saldo += valor;
-                System.out.println("Valor depositado com sucesso");
-                System.out.println("        Digite a operação desejada: \n" +
-                        "        1- Consultar saldos\n" +
-                        "        2- Depositar valor\n" +
-                        "        3- Transferir valor\n" +
-                        "        4- Sair\n" +
-                        "\n");
-                operacao = scanner.nextInt();
-            } else if (operacao == 3) {
-                System.out.println("Para qual conta gostaria de transferir?");
-                String account = scanner.next();
-                System.out.println("Qual o valor que gostaria de depositar?");
-                double valor = scanner.nextDouble();
-                if (valor > saldo) {
-                    System.out.println("Você não tem esse valor para transferir");
-                } else if (valor <= saldo) {
-                    saldo -= valor;
-                    System.out.println("Valor transferido com sucesso para a conta: " + account);
-                } else if (operacao != 1 || operacao != 2 || operacao != 3 || operacao != 4) {
-                    System.out.println("Opção inválida, tente novamente");
-                }
-            } if (operacao == 4) {
-                System.out.println("Agradecemos a preferência, volte sempre!");
-
         }
+        System.out.println("Obrigado pela preferência, volte sempre.");
     }
-}}}
+}
+
+
+
 
